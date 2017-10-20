d6c797cc299437e08d1a38ee424243684683168a
zhaojw <zhaojw_420@163.com>
2017-10-20 10:14:23 +0800

    mybatis自动生成

--- springboot-mybatis/src/main/java/com/springboot/mybatis/SpringbootMybatisApplication.java
+++ springboot-mybatis/src/main/java/com/springboot/mybatis/SpringbootMybatisApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.myabtis;
+package com.springboot.mybatis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
