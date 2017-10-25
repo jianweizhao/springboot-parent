957f0af3aa648d604139763d26e83f2b862f14a7
zhaojw <zhaojw_420@163.com>
2017-10-25 14:10:23 +0800

    springboot整合shiro实现单点登录

---
+++ springboot-shiro/src/main/java/com/springboot/shiro/entity/Role.java
@@ -0,0 +1,47 @@
+package com.springboot.shiro.entity;
+
+import java.io.Serializable;
+
+public class Role implements Serializable {
+    private Long id;
+
+    private String available;
+
+    private String description;
+
+    private String role;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(String available) {
+        this.available = available == null ? null : available.trim();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role == null ? null : role.trim();
+    }
+}
---
+++ springboot-shiro/src/main/java/com/springboot/shiro/entity/User.java
@@ -0,0 +1,67 @@
+package com.springboot.shiro.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private Long id;
+
+    private String username;
+
+    private String name;
+
+    private String password;
+
+    private String salt;
+
+    private Boolean state;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt == null ? null : salt.trim();
+    }
+
+    public Boolean getState() {
+        return state;
+    }
+
+    public void setState(Boolean state) {
+        this.state = state;
+    }
+}
